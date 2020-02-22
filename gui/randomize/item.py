6134cf70582762b8ac3d8b2b3af673895fe1a442
Mike A. Trethewey <miketrethewey@gmail.com>
2020-02-22 00:54:35 -0800

    Condense Item Rando options

--- gui/randomize/item.py
+++ gui/randomize/item.py
@@ -15,30 +15,21 @@ def item_page(parent):
 
     self.frames["checkboxes"] = Frame(self)
     self.frames["checkboxes"].pack(anchor=W)
-    with open(os.path.join("resources","app","gui","randomize","item","checkboxes.json")) as checkboxes:
-        myDict = json.load(checkboxes)
-        dictWidgets = widgets.make_widgets_from_dict(self, myDict, self.frames["checkboxes"])
-        for key in dictWidgets:
-            self.widgets[key] = dictWidgets[key]
-            self.widgets[key].pack(anchor=W)
 
     self.frames["leftItemFrame"] = Frame(self)
     self.frames["rightItemFrame"] = Frame(self)
     self.frames["leftItemFrame"].pack(side=LEFT)
     self.frames["rightItemFrame"].pack(side=RIGHT)
 
-    with open(os.path.join("resources","app","gui","randomize","item","leftItemFrame.json")) as leftItemFrameItems:
-        myDict = json.load(leftItemFrameItems)
-        dictWidgets = widgets.make_widgets_from_dict(self, myDict, self.frames["leftItemFrame"])
-        for key in dictWidgets:
-            self.widgets[key] = dictWidgets[key]
-            self.widgets[key].pack(anchor=E)
-
-    with open(os.path.join("resources","app","gui","randomize","item","rightItemFrame.json")) as rightItemFrameItems:
-        myDict = json.load(rightItemFrameItems)
-        dictWidgets = widgets.make_widgets_from_dict(self, myDict, self.frames["rightItemFrame"])
-        for key in dictWidgets:
-            self.widgets[key] = dictWidgets[key]
-            self.widgets[key].pack(anchor=E)
+    with open(os.path.join("resources","app","gui","randomize","item","widgets.json")) as widgetDefns:
+        myDict = json.load(widgetDefns)
+        for framename,theseWidgets in myDict.items():
+            dictWidgets = widgets.make_widgets_from_dict(self, theseWidgets, self.frames[framename])
+            for key in dictWidgets:
+                self.widgets[key] = dictWidgets[key]
+                packAttrs = {"anchor":E}
+                if self.widgets[key].type == "checkbox":
+                    packAttrs["anchor"] = W
+                self.widgets[key].pack(packAttrs)
 
     return self
