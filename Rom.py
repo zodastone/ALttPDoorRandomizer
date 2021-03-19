c663c9edaa22cd8a3b6becf8df41ca8df7e48a46
StructuralMike <66819228+StructuralMike@users.noreply.github.com>
2021-03-19 17:36:54 +0100

    Update Rom.py

--- Rom.py
+++ Rom.py
@@ -932,10 +932,10 @@ def patch_rom(world, rom, player, team, enemized, is_mystery=False):
         else:
             overflow_replacement = GREEN_TWENTY_RUPEES
 
-    difficulty = world.difficulty_requirements[player]
-
     #Byrna residual magic cost
     rom.write_bytes(0x45C42, [0x04, 0x02, 0x01])
+
+    difficulty = world.difficulty_requirements[player]
 
     #Set overflow items for progressive equipment
     rom.write_bytes(0x180090,
@@ -1138,8 +1138,8 @@ def patch_rom(world, rom, player, team, enemized, is_mystery=False):
     rom.write_byte(0x180171, 0x01 if world.ganon_at_pyramid[player] else 0x00)  # Enable respawning on pyramid after ganon death
     rom.write_byte(0x180173, 0x01) # Bob is enabled
     rom.write_byte(0x180168, 0x08)  # Spike Cave Damage
-    rom.write_bytes(0x18016B, [0x04, 0x02, 0x01])  # Set spike cave and MM spike room Byrna usage
-    rom.write_bytes(0x18016E, [0x04, 0x08, 0x10])  # Set spike cave and MM spike room Cape usage
+    rom.write_bytes(0x18016B, [0x04, 0x02, 0x01]) # Set spike cave and MM spike room Byrna usage
+    rom.write_bytes(0x18016E, [0x04, 0x08, 0x10]) # Set spike cave and MM spike room Cape usage
     rom.write_bytes(0x50563, [0x3F, 0x14]) # disable below ganon chest
     rom.write_byte(0x50599, 0x00) # disable below ganon chest
     rom.write_bytes(0xE9A5, [0x7E, 0x00, 0x24]) # disable below ganon chest
